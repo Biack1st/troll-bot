a4e5dc93f02cca6b321be36dc80f1a8f86ba8ce4
BIack1st <steveminecrap69@gmail.com>
2022-04-12 16:24:19 -0500

    updated

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
 const { Client, Intents, Guild, GuildMember } = require('discord.js');
-const { token } = require('./config.json');
+const keepAlive = require("./server")
+
+require("dotenv").config()
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('messageCreate', (msg) => {
-    try {
-        if (! msg.author.bot && msg.content.toLowerCase().match("!troll")) {
+      if (! msg.author.bot) {
+         if (msg.content.toLowerCase().match("!troll")) {
+            msg.delete()
             const target = msg.content.split(" ")[1]
 
             const trollMsg = msg.content.split("-m")[1] || "get trolled nub lmao"
-
-            console.log(trollMsg)
-
+              
+            const anonymous = msg.content.split("-a")[1] != null || false
+                        
             if (target) {
                 const member = client.users.cache.find(u => u.tag == target)
     
                 const currentUser = msg.author.tag
     
-                if (member) {
+                if (member && ! member.bot) {
                     const id = member.id
                     
                     const targetUser = client.users.cache.find(u => u.id == id)
     
-                    targetUser.send(`${currentUser} trolled you: ${trollMsg}`)
+                    targetUser.send(`${ anonymous == true && "Anonymous" || currentUser} trolled you: ${trollMsg}`)
     
                     console.log(`sent msg to ${targetUser}`)
                 }
             }
         }
-    }
-    catch(err) {
-        console.log(`app crashed for reason of: ${err}`)
-    }
+      }
+       
 })
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(token);
+
+keepAlive()
+
+client.login(process.env.TOKEN);
